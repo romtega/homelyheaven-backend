be99fbd5e288212294bff63d694d706d09baaf07
arano1212 <arano1212@gmail.com>
2024-05-30 15:32:26 -0600

    services and place

--- models/housing.js
+++ models/housing.js
@@ -1,12 +1,23 @@
 import mongoose from 'mongoose'
 
+const placeEnum = ['beach', 'city', 'village', 'mountain', 'suburb', 'desert']
+
 const housingSchema = new mongoose.Schema({
   name: { type: String, required: true },
   type: { type: String, required: true },
   price: { type: Number, required: true },
   bedrooms: { type: Number, required: true },
   bathrooms: { type: Number, required: true },
+  place: { type: String, enum: placeEnum },
   description: { type: String },
+  services: {
+    wifi: { type: Boolean, default: false },
+    parking: { type: Boolean, default: false },
+    pool: { type: Boolean, default: false },
+    airConditioning: { type: Boolean, default: false },
+    heating: { type: Boolean, default: false },
+    kitchen: { type: Boolean, default: false }
+  },
   address: { type: mongoose.Schema.Types.ObjectId, ref: 'Address', required: true },
   imgUrl: { type: [String], default: [] },
   isActive: { type: Boolean, default: true }
