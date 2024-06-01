1839838fe6692e2c5730612f4d8bc3f95e8c0245
arano1212 <arano1212@gmail.com>
2024-06-01 14:46:38 -0600

    maps

--- models/address.js
+++ models/address.js
@@ -6,6 +6,8 @@ const addressSchema = new mongoose.Schema({
   state: { type: String, required: true },
   postalCode: { type: String, required: true, match: /^[0-9]{5}$/ },
   country: { type: String, required: true },
+  latitude: { type: Number, required: true },
+  longitude: { type: Number, required: true },
   isActive: { type: Boolean, default: true }
 
 }, { timestamps: true })
